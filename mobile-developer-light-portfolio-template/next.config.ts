579b27344f0d0c3eaaa7109e0e7147de310e9aee
louick <louickmytc@gmail.com>
2025-06-11 19:45:06 -0300

    ajuste

--- mobile-developer-light-portfolio-template/next.config.ts
+++ mobile-developer-light-portfolio-template/next.config.ts
@@ -1,7 +1,17 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  // gera saída estática em out/
+  output: "export",
+
+  // quando em produção, aplica o basePath para GitHub Pages
+  basePath: isProd ? "/louick-portifolio" : "",
+  assetPrefix: isProd ? "/louick-portifolio/" : "",
 };
 
 export default nextConfig;
